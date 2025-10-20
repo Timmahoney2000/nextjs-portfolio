cf4107d9c009387a532af0f8dc22bc776d39b884
Tim Mahoney <timmahoney2000@gmail.com>
2025-10-20 15:35:29 -0400

    updated recent projects

--- data/projects.ts
+++ data/projects.ts
@@ -15,20 +15,20 @@ export const projects = [
         projectPreviewLink: "https://sonic-side-scroller.netlify.app/",
         projectSourceCode: "https://github.com/Timmahoney2000/sonic"
     },
-    {
+     {
          id: 2,
-        projectTitle: "Sonic the Hedgehog side-scroller web game",
-        projectDescription: "Sonic Side-Scroller is a 2D browser game built with Vite and JavaScript, featuring smooth animations, collision detection, and responsive controls. It showcases my ability to build interactive, performance-optimized experiences using modern front-end technologies",
-        projectImage: "/images/sonic.png",
-        projectPreviewLink: "https://sonic-side-scroller.netlify.app/",
-        projectSourceCode: "https://github.com/Timmahoney2000/sonic"
+        projectTitle: "Mern-Notes",
+        projectDescription: "A note app created using the MERN stack with drag and drop functionality",
+        projectImage: "/images/mern-notes.png",
+        projectPreviewLink: "https://mern-thinkboard-osn3.onrender.com/",
+        projectSourceCode: "https://github.com/Timmahoney2000/mern-thinkboard"
     },
-    {
+     {
          id: 2,
-        projectTitle: "Sonic the Hedgehog side-scroller web game",
-        projectDescription: "Sonic Side-Scroller is a 2D browser game built with Vite and JavaScript, featuring smooth animations, collision detection, and responsive controls. It showcases my ability to build interactive, performance-optimized experiences using modern front-end technologies",
-        projectImage: "/images/sonic.png",
-        projectPreviewLink: "https://sonic-side-scroller.netlify.app/",
-        projectSourceCode: "https://github.com/Timmahoney2000/sonic"
+        projectTitle: "Starry Night",
+        projectDescription: "A React component with a starry night background with a fade-in/fade-out effect",
+        projectImage: "/images/starrynight.png",
+        projectPreviewLink: "https://starrynight-component.netlify.app/",
+        projectSourceCode: "https://github.com/Timmahoney2000/StarryNight"
     }
 ];
