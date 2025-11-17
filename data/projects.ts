2192273cbec8f3bf03f401d22b6c02a826ed16f3
Tim Mahoney <timmahoney2000@gmail.com>
2025-11-16 19:44:36 -0500

    unicode issue

--- data/projects.ts
+++ data/projects.ts
@@ -1,14 +1,14 @@
 export const projects = [
     {
         id: 1,
-        projectTitle: "A custom seafood restaurant website highlighting Park Seafood’s menu, specials, and story through a clean, engaging design.",
-        projectDescription: "A modern, mobile-friendly restaurant site built to highlight Park Seafood’s menu and coastal charm with a clean design and smooth user experience.",
+        projectTitle: "A custom seafood restaurant website highlighting Park Seafood's menu, specials, and story through a clean, engaging design.",
+        projectDescription: "A modern, mobile-friendly restaurant site built to highlight Park Seafood's menu and coastal charm with a clean design and smooth user experience.",
         projectImage: "/images/park-seafood.png",
         projectPreviewLink: "https://parkseafood.netlify.app/",
         projectSourceCode: "https://github.com/Timmahoney2000/park_seafood"
     },
-        {
-         id: 3,
+    {
+        id: 3,
         projectTitle: "Mern-Notes",
         projectDescription: "A note app created using the MERN stack with drag and drop functionality",
         projectImage: "/images/mern-notes.png",
@@ -23,13 +23,14 @@ export const projects = [
         projectPreviewLink: "https://sonic-side-scroller.netlify.app/",
         projectSourceCode: "https://github.com/Timmahoney2000/sonic"
     },
-     {
-         id: 4,
+    {
+        id: 4,
         projectTitle: "Pizzeria E-Commerce Site",
-        projectDescription: "Ciccio’s Pizza is a pizzeria e-commerce website built with Next.js (bootstrapped via create-next-app). Using TypeScript and optimized font loading (via next/font), the site provides a modern, performant user experience. The repository is deployed on Vercel, making it easy to host and scale.",
+        projectDescription: "Ciccio's Pizza is a pizzeria e-commerce website built with Next.js (bootstrapped via create-next-app). Using TypeScript and optimized font loading (via next/font), the site provides a modern, performant user experience. The repository is deployed on Vercel, making it easy to host and scale.",
         projectImage: "/images/pizza.png",
         projectPreviewLink: "https://ciccios-pizza-git-master-tims-projects-43918398.vercel.app/",
         projectSourceCode: "https://github.com/Timmahoney2000/ciccios-pizza"
-     }
-    ];
+    }
+];
+
      
