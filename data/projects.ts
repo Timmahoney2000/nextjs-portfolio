bae20d6817f407155e763871a220dc0d8f240158
Tim Mahoney <timmahoney2000@gmail.com>
2026-01-03 14:25:55 -0500

    Update: reordered recent projects

--- data/projects.ts
+++ data/projects.ts
@@ -7,6 +7,14 @@ export const projects = [
         projectPreviewLink: "https://www.lawparse.com/",
         projectSourceCode: "https://github.com/Timmahoney2000/legalese"
     },
+       {
+        id: 2,
+        projectTitle: "100 Devs AI Resource Site",
+        projectDescription: "A curated resource platform built for the 100 Devs community, featuring AI-powered search that finds exactly what users need across videos and tutorials. The site includes direct timestamped video links based on search criteria, a personalized bookmarking system, and an organized library of tools, guides, and learning resources. Designed with a clean, intuitive interface.",
+        projectImage: "/images/100resources.png",
+        projectPreviewLink: "https://www.100resources.info/",
+        projectSourceCode: "https://github.com/Timmahoney2000/frontend"
+    },
     {
         id: 3,
         projectTitle: "Mern-Notes",
@@ -16,21 +24,14 @@ export const projects = [
         projectSourceCode: "https://github.com/Timmahoney2000/mern-thinkboard"
     },
     {
-        id: 2,
+        id: 4,
         projectTitle: "Sonic the Hedgehog side-scroller web game",
         projectDescription: "Sonic Side-Scroller is a 2D browser game built with Vite and JavaScript, featuring smooth animations, collision detection, and responsive controls. It showcases my ability to build interactive, performance-optimized experiences using modern front-end technologies",
         projectImage: "/images/sonic.png",
         projectPreviewLink: "https://sonic-side-scroller.netlify.app/",
         projectSourceCode: "https://github.com/Timmahoney2000/sonic"
     },
-    {
-        id: 4,
-        projectTitle: "100 Devs AI Resource Site",
-        projectDescription: "A curated resource platform built for the 100 Devs community, featuring AI-powered search that finds exactly what users need across videos and tutorials. The site includes direct timestamped video links based on search criteria, a personalized bookmarking system, and an organized library of tools, guides, and learning resources. Designed with a clean, intuitive interface.",
-        projectImage: "/images/100resources.png",
-        projectPreviewLink: "https://www.100resources.info/",
-        projectSourceCode: "https://github.com/Timmahoney2000/frontend"
-    }
 ];
+ 
 
      
